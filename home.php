b4d7df82d4b3c7ff54311e1e047755ed4b02f285
James Guerra <james.wylie.guerra@gmail.com>
2015-06-08 02:44:27 -0400

    added class functionality on home

--- home.php
+++ home.php
@@ -1,8 +1,24 @@
 <?php
 session_start();
+include 'myInfo.php';
 if(!isset($_SESSION['logged-in']))
 {
 	header("Location: login.php");
 }
+class t
+{
+	public $s_id;
+	public $s_name;
+	public $s_rate;
+	public $s_review;
+	public $s_grade;
+	public $s_date;
+	public $s_online;
+	function oprint(){
+		echo $this->s_id . $this->s_name . $this->s_cat . $this->s_length . $this->s_rented;
+	}
+	
+	
+	
 echo 'home!';
 ?>
